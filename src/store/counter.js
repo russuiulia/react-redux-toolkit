ffbd0a4c686be896283ffa9ecb4a6bec9d8da2dc
russuiulia <russuiulia.md@gmail.com>
2022-07-11 13:43:35 +0300

    first-commit

---
+++ src/store/counter.js
@@ -0,0 +1,27 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { createStore } from 'redux';
+const initialCounterState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+
+  name: 'counter',
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter
+    },
+  }
+});
+
+export const counterActions = counterSlice.actions;
+
+export default counterSlice.reducer;
